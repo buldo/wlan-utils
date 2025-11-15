ba3d1004a4146daac656858460e98cc61fbd38d9
Roman Buldygin <froooks@gmail.com>
2025-11-15 23:47:29 +0300

    save

--- src/Bld.Libnl/NetlinkAttributeExtensions.cs
+++ src/Bld.Libnl/NetlinkAttributeExtensions.cs
@@ -8,41 +8,42 @@ public static unsafe class NetlinkAttributeExtensions
 {
     /// <summary>
     /// Iterate over nested attributes
-    /// Equivalent to nla_for_each_nested(pos, nla, rem) macro
+    /// Equivalent to nla_for_each_nested(pos, nla, rem) macro:
+    /// for (pos = nla_data(nla), rem = nla_len(nla); nla_ok(pos, rem); pos = nla_next(pos, &rem))
     /// </summary>
     /// <param name="nla">Attribute containing the nested attributes</param>
     /// <returns>Enumerable sequence of nested attribute pointers</returns>
     public static IEnumerable<IntPtr> EnumerateNested(this IntPtr nla)
     {
-        if (nla == IntPtr.Zero)
-            return Enumerable.Empty<IntPtr>();
+        // Direct translation: pos = nla_data(nla), rem = nla_len(nla)
+        var pos = LibNlNative.nla_data(nla);
+        var rem = LibNlNative.nla_len(nla);
 
-        var data = LibNlNative.nla_data(nla);
-        var len = LibNlNative.nla_len(nla);
-
-        return EnumerateAttributes(data, len);
+        return EnumerateAttributesImpl(pos, rem);
     }
 
     /// <summary>
     /// Iterate over attributes in a stream
-    /// Equivalent to nla_for_each_attr(pos, head, len, rem) macro
+    /// Equivalent to nla_for_each_attr(pos, head, len, rem) macro:
+    /// for (pos = head, rem = len; nla_ok(pos, rem); pos = nla_next(pos, &rem))
     /// </summary>
     /// <param name="head">Head of attribute stream</param>
     /// <param name="len">Length of attribute stream</param>
     /// <returns>Enumerable sequence of attribute pointers</returns>
     public static IEnumerable<IntPtr> EnumerateAttributes(IntPtr head, int len)
     {
-        if (head == IntPtr.Zero || len <= 0)
-            return Enumerable.Empty<IntPtr>();
+        return EnumerateAttributesImpl(head, len);
+    }
 
+    private static IEnumerable<IntPtr> EnumerateAttributesImpl(IntPtr pos, int rem)
+    {
+        // Direct translation of: for (; nla_ok(pos, rem); pos = nla_next(pos, &rem))
         var result = new List<IntPtr>();
-        var remaining = len;
-        var current = head;
 
-        while (LibNlNative.nla_ok(current, remaining))
+        while (LibNlNative.nla_ok(pos, rem))
         {
-            result.Add(current);
-            current = LibNlNative.nla_next(current, &remaining);
+            result.Add(pos);
+            pos = LibNlNative.nla_next(pos, &rem);
         }
 
         return result;
